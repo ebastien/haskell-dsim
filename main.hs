2d8a1943cad1653b53a341f68e1f246a00357881
Emmanuel Bastien <os@ebastien.name>
2011-08-11 17:13:47 +0200

    Still trying to come up with the correct simulation model.

--- main.hs
+++ main.hs
@@ -1,6 +1,8 @@
 import DSim.Inventory
+
 import Data.Time.Calendar
 import Data.Time.LocalTime
+import Data.Time.Clock
 
 import Random
 
@@ -27,31 +29,69 @@ type Fare = Double
 
 type Availability = Double
 
-data Event = BookingRequestEvent BookingRequest
-
-type Demand = [Event]
-
-data Snapshot = VoidSnapshot
-
-generateEvent :: (RandomGen gen) => gen -> (Event, gen)
-generateEvent gen = (BookingRequestEvent BookingRequest, gen)
-
-generateDemand :: (RandomGen gen) => gen -> Demand
-generateDemand gen0 = let (e, gen1) = generateEvent gen0 in e:generateDemand gen1
-
 simulateBookingRequest :: BookingRequest -> TravelSolution
 simulateBookingRequest br = TravelSolution
 
 findTravelSolutions :: BookingRequest -> TravelSolutionsList
 findTravelSolutions br = []
 
-runEvent :: Event -> Snapshot -> Snapshot
-runEvent e s = s
+data EventType = VoidEvent | BookingEvent BookingRequest deriving (Show)
 
-runSimulation :: Demand -> Snapshot -> Snapshot
-runSimulation d s0 = foldr runEvent s0 d
+data Event = Event { eventTime :: UTCTime
+                   , eventType :: EventType
+                   } deriving (Show)
 
-main = do
-  gen <- getStdGen
-  let demand = take 10 $ generateDemand gen
-  return $ runSimulation demand VoidSnapshot
+type Demand = [Event]
+
+data Snapshot = Snapshot { snapTime :: UTCTime
+                         , snapCounter :: Int
+                         } deriving (Show)
+
+type Simulation = [Snapshot]
+
+data BreakPoint = NeverBreak | TimeBreak { breakTime :: UTCTime } deriving (Show)
+
+-- | Generate a single event
+
+generateEvent :: (RandomGen gen) => gen -> Event -> (Event, gen)
+generateEvent gen0 evt0 = (Event newTime newType, gen0)
+                        where
+                          newTime = addUTCTime (fromInteger 10) (eventTime evt0)
+                          newType = BookingEvent BookingRequest
+
+-- | Generate the demand (i.e an infinite sequence of events)
+
+generateDemand :: (RandomGen gen) => gen -> Event -> Demand
+generateDemand gen0 evt0 = let (evt1, gen1) = generateEvent gen0 evt0 in evt1:generateDemand gen1 evt1
+
+-- | Apply a single event on a simulation snapshot
+
+applyEvent :: Snapshot -> Event -> Snapshot
+applyEvent (Snapshot tp c) (Event te _) = Snapshot te (c+1)
+
+-- | Test whether a breakpoint has been reached
+
+breakReached :: BreakPoint -> Snapshot -> Bool
+breakReached NeverBreak    _ = False
+breakReached (TimeBreak t) p = t <= (snapTime p)
+
+-- | Simulate a demand starting from an initial snapshot
+
+runSimulation :: Snapshot -> Demand -> Simulation
+runSimulation p0 d = foldr step [p0] d
+                   where
+                     step e s = (applyEvent (head s) e):s
+
+-- | Run a simulation until the breakpoint is reached
+
+runUntil :: BreakPoint -> Snapshot -> Demand -> Simulation
+runUntil b p0 d = takeWhile (not . breakReached b) $ runSimulation p0 d
+
+--
+
+tzero = UTCTime (fromGregorian 2000 01 01) (timeOfDayToTime (TimeOfDay 00 00 00))
+tdone = UTCTime (fromGregorian 2000 01 01) (timeOfDayToTime (TimeOfDay 00 01 00))
+snap = Snapshot tzero 0
+bkpt = TimeBreak tdone
+demand = generateDemand (mkStdGen 123) (Event tzero VoidEvent)
+result = runUntil bkpt snap demand
