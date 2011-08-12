5f987815db85533299139dda312c73c3a30d22c4
Emmanuel Bastien <os@ebastien.name>
2011-08-13 00:47:56 +0200

    Strict simulation scan and faster time type.

--- main.hs
+++ main.hs
@@ -37,31 +37,33 @@ findTravelSolutions br = []
 
 data EventType = VoidEvent | BookingEvent BookingRequest deriving (Show)
 
-data Event = Event { eventTime :: UTCTime
+type SimTime = NominalDiffTime
+
+data Event = Event { eventTime :: SimTime
                    , eventType :: EventType
                    } deriving (Show)
 
 type Demand = [Event]
 
-data Snapshot = Snapshot { snapTime :: UTCTime
+data Snapshot = Snapshot { snapTime :: SimTime
                          , snapCounter :: Int
                          } deriving (Show)
 
 type Simulation = [Snapshot]
 
-data BreakPoint = TimeBreak { breakTime :: UTCTime } deriving (Show)
+data BreakPoint = TimeBreak { breakTime :: SimTime } deriving (Show)
 
 -- | Generate a single event
 
-generateEvent :: (RandomGen gen) => gen -> UTCTime -> (Event, gen)
+generateEvent :: (RandomGen gen) => gen -> SimTime -> (Event, gen)
 generateEvent gen0 t0 = (Event t1 be, gen0)
                       where
-                        t1 = (fromInteger 10) `addUTCTime` t0
-                        be = BookingEvent BookingRequest
+                        t1 = {-# SCC "A" #-} t0 + 10
+                        be = {-# SCC "B" #-} BookingEvent BookingRequest
 
 -- | Generate the demand (i.e an infinite sequence of events)
 
-generateDemand :: (RandomGen gen) => gen -> UTCTime -> Demand
+generateDemand :: (RandomGen gen) => gen -> SimTime -> Demand
 generateDemand gen0 t0 = e1 : generateDemand gen1 (eventTime e1)
                        where
                          (e1, gen1) = generateEvent gen0 t0
@@ -76,10 +78,17 @@ applyEvent (Snapshot tp c) (Event te _) = Snapshot te (c+1)
 bkptReached :: BreakPoint -> Snapshot -> Bool
 bkptReached (TimeBreak t) p = t <= (snapTime p)
 
+-- | Strict version of scanl
+
+scanl' :: (a -> b -> a) -> a -> [b] -> [a]
+scanl' f q ls =  q : (case ls of
+                      []   -> []
+                      x:xs -> let q' = f q x in q' `seq` scanl' f q' xs)
+
 -- | Simulate a demand starting from an initial snapshot
 
 runSimulation :: Snapshot -> Demand -> Simulation
-runSimulation p0 d = scanl applyEvent p0 d
+runSimulation p0 d = scanl' applyEvent p0 d
 
 -- | Keep simulation results until a breakpoint is reached
 
@@ -95,12 +104,14 @@ keepBetween b1 b2 = dropWhile (not . bkptReached b1) . keepUntil b2
 t_zero = UTCTime (fromGregorian 2000 01 01) (timeOfDayToTime (TimeOfDay 00 00 00))
 
 -- The initial state of the simulation
-s_zero = Snapshot t_zero 0
+s_zero = Snapshot 0 0
 
-demand = generateDemand (mkStdGen 123) t_zero
+demand = generateDemand (mkStdGen 123) 0
 
-b1 = TimeBreak $ UTCTime (fromGregorian 2000 03 01) (timeOfDayToTime (TimeOfDay 00 10 00))
-b2 = TimeBreak $ UTCTime (fromGregorian 2000 03 01) (timeOfDayToTime (TimeOfDay 00 11 00))
+t1 = UTCTime (fromGregorian 2000 03 01) (timeOfDayToTime (TimeOfDay 00 10 00))
+t2 = UTCTime (fromGregorian 2000 03 01) (timeOfDayToTime (TimeOfDay 00 11 00))
+b1 = TimeBreak $ diffUTCTime t1 t_zero
+b2 = TimeBreak $ diffUTCTime t2 t_zero
 
 result = keepBetween b1 b2 $ runSimulation s_zero demand
 
