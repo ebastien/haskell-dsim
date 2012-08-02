f10d434a552b9e4c33a9eaa1212e6bf2a125c4e7
Emmanuel Bastien <os@ebastien.name>
2012-08-02 17:38:04 +0200

    Parsing of SSIM structure.

--- cdt.hs
+++ cdt.hs
@@ -7,28 +7,9 @@ import System.Environment (getArgs)
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.List as CL
 import qualified Filesystem.Path.CurrentOS as FP
-
 import Text.Regex.TDFA ((=~))
-
 import Control.Concurrent (threadDelay)
-
-import Debug.Trace (trace)
-import Criterion.Main
-
 import Data.ByteString (ByteString)
-import qualified Data.ByteString as B
-
-import Data.Attoparsec.Types (Parser)
-import qualified Data.Attoparsec.ByteString.Char8 as P
-import Data.Attoparsec.Combinator (choice)
-import Data.Conduit.Attoparsec (conduitParser)
-
-import qualified Data.ByteString.Lazy as LB
-import qualified Data.Attoparsec.ByteString.Lazy as LP
-
-import Data.Maybe (fromMaybe)
-import Data.Functor ((<$>))
-import Control.Applicative (some)
 
 count :: Monad m => Sink ByteString m Int
 count = CB.lines =$ CL.fold (\n _ -> n + 1 ) 0
@@ -43,43 +24,3 @@ readAll = sourceArgs
             sourceDir f = traverse False (FP.decodeString f)
             filePattern f = (FP.encodeString f) =~ ("\\.html$" :: String)
             slowly x = liftIO (threadDelay 10000) >> return x
-
-type Airline = ByteString
-
-data Record = Header
-            | Carrier { carAirline :: Airline }
-            | Leg
-            | Segment
-            | Trailer
-            | Empty deriving Show
-
-recordParser = choice [header, carrier, leg, segment, trailer, empty]
-  where header  = P.char '1' >> P.take 199 >> P.endOfLine >> return Header
-        carrier = do
-          P.char '2'
-          P.satisfy (\c -> c == 'U' || c == 'L')
-          ad <- P.take 3
-          P.take 195
-          P.endOfLine
-          return (Carrier ad)
-        leg     = P.char '3' >> P.take 199 >> P.endOfLine >> return Leg
-        segment = P.char '4' >> P.take 199 >> P.endOfLine >> return Segment
-        trailer = P.char '5' >> P.take 199 >> P.endOfLine >> return Trailer
-        empty   = P.endOfLine >> return Empty
-
-conduitRecord = conduitParser recordParser =$= CL.map snd
-
-resultConduit :: FP.FilePath -> IO [Record]
-resultConduit s = runResourceT $ sourceFile s $= conduitRecord $$ CL.take 10
-
-resultLazy :: FP.FilePath -> IO [Record]
-resultLazy s = do
-  c <- LB.readFile $ FP.encodeString s
-  let r = LP.parse (some recordParser) c
-  return . take 10 . fromMaybe [] $ LP.maybeResult r
-
-main = defaultMain [
-  bench "conduit" $ whnfIO $ length <$> resultConduit input,
-  bench "lazy" $ whnfIO $ length <$> resultLazy input
-  ]
-  where input = "oag.ssim7.head"
--- oag.hs
+++ oag.hs
@@ -1,22 +1,68 @@
-import Prelude hiding (take) -- conflicting with Attoparsec
-import Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Lazy.Char8 as LB (ByteString, getContents)
-import Data.Attoparsec.ByteString.Char8 (take, Parser)
-import Data.Attoparsec.ByteString.Lazy (maybeResult, parse, Result (Done))
+{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction #-}
 
-data LegElem = LegElem {
-  legAirline :: !ByteString
-} deriving Show
+import qualified Filesystem.Path.CurrentOS as FP
 
-parseElem :: Parser LegElem
-parseElem = do
-  a <- take 2
-  return $ LegElem a
+import Criterion.Main
 
-go :: LB.ByteString -> Maybe LegElem
-go = maybeResult . (parse parseElem)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 
-main :: IO ()
-main = do
-  c <- LB.getContents
-  print $ go c
+import Data.Attoparsec.Types (Parser)
+import qualified Data.Attoparsec.ByteString.Char8 as P
+
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.Attoparsec.ByteString.Lazy as LP
+
+import Data.Functor ((<$>))
+import Control.Applicative (some, many, (<*>), (<*))
+
+type Designator = ByteString
+
+data Header = Header deriving Show
+
+data Carrier = Carrier { carAirline :: Designator } deriving Show
+
+data Leg = Leg deriving Show
+
+data Segment = Segment deriving Show
+
+data LegGroup = LegGroup { lgLeg :: Leg
+                         , lgSegments :: [Segment] } deriving Show
+
+data CarrierGroup = CarrierGroup { cgCarrier :: Carrier
+                                 , cgLegs :: [LegGroup] } deriving Show
+
+data Ssim = Ssim { ssimHeader :: Header
+                 , ssimCarriers :: [CarrierGroup] } deriving Show
+
+headerParser  = P.char '1' >> P.take 199 >> (some P.endOfLine) >> return Header
+
+carrierParser = do
+  P.char '2'
+  P.satisfy (\c -> c == 'U' || c == 'L')
+  ad <- P.take 3
+  P.take 195
+  some P.endOfLine
+  return (Carrier ad)
+
+legParser     = P.char '3' >> P.take 199 >> (some P.endOfLine) >> return Leg
+
+segmentParser = P.char '4' >> P.take 199 >> (some P.endOfLine) >> return Segment
+
+trailerParser = P.char '5' >> P.take 199 >> (some P.endOfLine) >> return ()
+
+legGroupParser = LegGroup <$> legParser <*> (many segmentParser)
+
+carrierGroupParser = CarrierGroup <$> carrierParser <*> (many legGroupParser)
+
+ssimParser = Ssim <$> headerParser <*> (many carrierGroupParser) <* trailerParser
+
+resultLazy :: FP.FilePath -> IO (LP.Result Ssim)
+resultLazy s = do
+  c <- LB.readFile $ FP.encodeString s
+  return $ LP.parse ssimParser c
+
+main = defaultMain [
+  bench "lazy" $ whnfIO $ resultLazy input
+  ]
+  where input = "oag.ssim7.sample"
