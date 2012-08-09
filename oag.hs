f4a4c2c13a33d4e99d0c2db2f11d83f4eee96738
Emmanuel Bastien <os@ebastien.name>
2012-08-09 16:52:48 +0200

    More parsers.

--- oag.hs
+++ oag.hs
@@ -1,10 +1,9 @@
-{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 import qualified Filesystem.Path.CurrentOS as FP
 
 import Criterion.Main
 
-import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy as LB
 
@@ -13,39 +12,54 @@ import qualified Data.Attoparsec.ByteString.Char8 as P
 import qualified Data.Attoparsec.ByteString.Lazy as LP
 
 import Data.Functor ((<$>))
-import Control.Applicative (pure, some, many, (<*>), (<*), (*>), (<|>))
+import Control.Monad (void)
+import Control.Applicative (pure, some, (<*>), (<*), (*>), (<|>))
 
 import Data.Maybe (fromMaybe)
 
+import Data.Time.LocalTime (TimeOfDay, makeTimeOfDayValid)
 import Data.Time.Calendar (Day, fromGregorianValid)
 
 import Data.List (elemIndex)
 
 import Data.Word (Word8)
-import Data.Bits (unsafeShiftL, (.|.), bit)
-import Data.Char (ord, chr)
+import Data.Bits (bit)
+import Data.Char (ord)
 
-type Designator = ByteString
 type PeriodBoundary = Maybe Day
 type Dow = Word8
 type Port = Int
+type AirlineCode = Int
 
 data Header = Header deriving Show
 
-data Carrier = Carrier { carAirline :: Designator 
+data Carrier = Carrier { cAirline :: AirlineCode 
                        } deriving Show
 
-data Leg = Leg { lgSuffix :: Char
-               , lgAirline :: Designator
-               , lgNumber :: Int
-               , lgBegin :: PeriodBoundary
-               , lgEnd :: PeriodBoundary
-               , lgDow :: Dow
+data Flight = Flight { fAirline :: AirlineCode
+                     , fNumber :: Int
+                     , fSuffix :: Char
+                     } deriving Show
+
+data Period = Period { pBegin :: PeriodBoundary
+                     , pEnd :: PeriodBoundary
+                     , pDow :: Dow
+                     } deriving Show
+
+data Leg = Leg { lSequence :: Int
+               , lBoard :: Port
+               , lOff :: Port
                } deriving Show
+
+data LegPeriod = LegPeriod { lpFlight :: Flight
+                           , lpPeriod :: Period
+                           , lpVariation :: Int
+                           , lpLeg :: Leg
+                           } deriving Show
 
 data Segment = Segment deriving Show
 
-data LegGroup = LegGroup { lgLeg :: Leg
+data LegGroup = LegGroup { lgLeg :: LegPeriod
                          , lgSegments :: [Segment] } deriving Show
 
 data CarrierGroup = CarrierGroup { cgCarrier :: Carrier
@@ -54,46 +68,52 @@ data CarrierGroup = CarrierGroup { cgCarrier :: Carrier
 data Ssim = Ssim { ssimHeader :: Header
                  , ssimCarriers :: [CarrierGroup] } deriving Show
 
+headerP :: Parser Header
 headerP  = P.char '1' >> P.take 199 >> (some P.endOfLine) >> return Header
 
+carrierP :: Parser Carrier
 carrierP = do
-  P.char '2'
-  P.satisfy (\c -> c == 'U' || c == 'L')
+  void $ P.char '2'
+  void $ P.satisfy (\c -> c == 'U' || c == 'L')
   airline <- airlineP
-  P.take 195
-  some P.endOfLine
+  void $ P.take 195
+  void $ some P.endOfLine
   return (Carrier airline)
 
-legP = do
-  P.char '3'                <?> "Leg record type"
+legPeriodP :: Parser LegPeriod
+legPeriodP = do
+  void (P.char '3')         <?> "Leg record type"
   suffix <- P.anyChar
   airline <- airlineP
   fnum <- fnumP             <?> "Leg flight number"
   iviL <- decimalP 2        <?> "Leg itinerary variation identifier (low)"
   lsn <- decimalP 2         <?> "Leg sequence number"
-  P.anyChar
+  void $ P.anyChar
   bdate <- periodBoundaryP  <?> "Leg period of operation (from)"
   edate <- periodBoundaryP  <?> "Leg period of operation (to)"
   dow <- dowP               <?> "Leg days of week"
-  P.anyChar
-  bpoint <- P.take 3
-  P.take 4
+  void $ P.anyChar
+  bpoint <- portP           <?> "Leg board point"
+  void $ P.take 4
   dtime <- P.take 4
   dutc <- P.take 5
-  P.take 2
-  opoint <- P.take 3
+  void $ P.take 2
+  opoint <- portP           <?> "Leg off point"
   atime <- P.take 4
-  P.take 4
+  void $ P.take 4
   autc <- P.take 5
-  P.take (2+3+20+5+10+9+2+6)
+  void $ P.take (2+3+20+5+10+9+2+6)
   iviH <- decimalP 1        <?> "Leg itinerary variation identifier (high)"
-  P.take (3+3+3+9+1+1+1+11+1+11+20)
+  void $ P.take (3+3+3+9+1+1+1+11+1+11+20)
   dvar <- P.anyChar
   avar <- P.anyChar
-  P.take 6
-  some P.endOfLine
-  let pnum = iviL + 100 * iviH
-  return (Leg suffix airline fnum bdate edate dow)
+  void $ P.take 6
+  void $ some P.endOfLine
+  let variation = iviL + 100 * iviH
+      flight = Flight airline fnum suffix
+      period = Period bdate edate dow
+      leg = Leg lsn bpoint opoint 
+  return $ LegPeriod flight period variation leg
 
 -- | Parser for fixed length decimal numbers with space padding.
 decimalP :: Int -> Parser Int
@@ -104,9 +124,15 @@ decimalP n = do
   fromMaybe (fail ("Decimal parsing failed on " ++ show s))
           $ (return . fst) <$> i
 
+-- | Parser for fixed length decimal numbers.
+decimalP' :: Int -> Parser Int
+decimalP' n = do
+  i <- B8.readInt <$> P.take n
+  fromMaybe (fail "Decimal parsing failed") $ (return . fst) <$> i
+
 -- | Parser for days.
 dayP :: Parser Int
-dayP = decimalP 2
+dayP = decimalP' 2
 
 -- | Parser for months.
 monthP :: Parser Int
@@ -118,7 +144,7 @@ monthP = do
 
 -- | Parser for years.
 yearP :: Num a => Parser a
-yearP = (fromIntegral . (2000+)) <$> decimalP 2
+yearP = (fromIntegral . (2000+)) <$> decimalP' 2
 
 -- | Parser for period boundaries.
 periodBoundaryP :: Parser PeriodBoundary
@@ -130,37 +156,66 @@ periodBoundaryP = do
                  $ (return . Just) <$> (fromGregorianValid y m d)
 
 -- | Parser for flight numbers.
+fnumP :: Parser Int
 fnumP = decimalP 4
 
--- | Parser for airline designators.
-airlineP = P.take 3
+-- | Parser for airline codes.
+airlineP :: Parser AirlineCode
+airlineP = sum <$> (sequence $ map step [0,1,2 :: Int]) <?> "Airline code"
+  where step n = (* 37^n) <$> code n
+        code n | n < 2     = letter <|>  digit
+               | otherwise = letter <|>  digit <|> space
+        letter = (+11) . subtract (ord 'A') . ord <$> P.satisfy (\c -> c >= 'A' && c <= 'Z')
+        digit  =  (+1) . subtract (ord '0') . ord <$> P.digit
+        space  = pure 0 <* P.space
 
 -- | Parser for days of week.
 dowP :: Parser Dow
-dowP = sum <$> (sequence $ step <$> ['1'..'7']) <?> "Days of week"
+dowP = sum <$> (sequence $ map step ['1'..'7']) <?> "Days of week"
   where step n = P.char n   *> (pure . bit $ ord n - ord '1')
              <|> P.char ' ' *> (pure 0)
 
 -- | Parser for ports.
 portP :: Parser Port
-portP = sum <$> (sequence $ step <$> [0,1,2]) <?> "Port"
+portP = sum <$> (sequence $ map step [0,1,2 :: Int]) <?> "Port"
   where step n = (* 26^n) . subtract (ord 'A') . ord <$> P.satisfy letter
         letter c = c >= 'A' && c <= 'Z'
 
-segmentP = P.char '4' >> P.take 199 >> (some P.endOfLine) >> return Segment
+-- | Parser for local times.
+localTimeP :: Parser TimeOfDay
+localTimeP = do
+  m <- makeTimeOfDayValid <$> decimalP' 2 <*> decimalP' 2 <*> pure 0
+  fromMaybe (fail "Local time parsing failed") $ return <$> m
 
-trailerP = do
-  P.char '5'       <?> "Trailer record type"
-  P.take 199       <?> "Trailer padding"
-  some P.endOfLine <?> "Trailer separator"
-  return ()
+-- | Parser for time variations.
+timeVariationP :: Parser Int
+timeVariationP = (plus <|> minus) <*> time
+  where plus = P.char '+' *> pure id
+        minus = P.char '-' *> pure negate
+        time = (+) <$> ((*60) <$> decimalP' 2) <*> decimalP' 2
 
-legGroupP = LegGroup <$> (legP          <?> "Leg record")
+segmentP :: Parser Segment
+segmentP = do
+  void $ P.char '4'
+  void $ P.take 199
+  void $ some P.endOfLine
+  return Segment
+
+trailerP :: Parser ()
+trailerP = (P.char '5'       <?> "Trailer record type")
+        *> (P.take 199       <?> "Trailer padding")
+        *> (some P.endOfLine <?> "Trailer separator")
+        *> pure ()
+
+legGroupP :: Parser LegGroup
+legGroupP = LegGroup <$> (legPeriodP    <?> "Leg record")
                      <*> (some segmentP <?> "Leg segments")
 
+carrierGroupP :: Parser CarrierGroup
 carrierGroupP = CarrierGroup <$> (carrierP       <?> "Carrier record")
                              <*> (some legGroupP <?> "Carrier legs")
 
+ssimP :: Parser Ssim
 ssimP = Ssim <$> (headerP            <?> "SSIM7 header")
              <*> (some carrierGroupP <?> "SSIM7 carriers")
              <*  (trailerP           <?> "SSIM7 trailer")
@@ -170,6 +225,7 @@ resultLazy s = do
   c <- LB.readFile $ FP.encodeString s
   return $ LP.parse ssimP c
 
+main :: IO ()
 main = defaultMain [
   bench "lazy" $ whnfIO $ resultLazy input
   ]
